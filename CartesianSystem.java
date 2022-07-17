87072f5c59d7964fd6cace91f1338cdfe2671df4
Akash <akhvnd@gmail.com>
2022-07-17 22:20:06 +0530

    Welcome to line comparison

---
+++ CartesianSystem.java
@@ -0,0 +1,15 @@
+package com.lineSystem.java;
+
+import java.util.Scanner;
+
+//import java.lang.Math.*;
+public class CartesianSystem {
+
+    //int x1,x2,y1,y2;	
+
+	public static void main(String[] args) {
+		
+	 System.out.println("Welcome to Line Comparison Computation ");
+	}  
+
+}
