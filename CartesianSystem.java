6792f8f4621c568a986cb919bd8a622876e2d31b
Sameeksha0031 <108124525+Sameeksha0031@users.noreply.github.com>
2022-07-17 23:26:02 +0530

    Update CartesianSystem.java

--- CartesianSystem.java
+++ CartesianSystem.java
@@ -3,39 +3,34 @@ package com.lineSystem.java;
 import java.util.Scanner;
 
 public class CartesianSystem {
-
+   
+	Scanner sc = new Scanner(System.in);
+	
+	public Double input() {
+		 System.out.println("Enter the point for x1 :");
+		 double x1 = sc.nextDouble();
+		 System.out.println("Enter the point for y1 :");
+		 double y1 = sc.nextDouble();
+		 System.out.println("Enter the point for x2 :");
+		 double x2 = sc.nextDouble();
+		 System.out.println("Enter the point for y2 :");
+		 double y2 = sc.nextDouble();
+		 return Calculate_length(x1,y1,x1,x2);
+	}
    
 	public static void main(String[] args) {
 		
 	 System.out.println("Welcome to Line Comparison Computation ");
-	 Scanner sc = new Scanner(System.in);
+	
 	 CartesianSystem obj = new CartesianSystem();
 	 CartesianSystem obj1 = new CartesianSystem();
 	 Double result1, result2;
 	 System.out.println("Line 1 points");
-	 System.out.println("Enter the point for x1 :");
-	 double x1 = sc.nextDouble();
-	 System.out.println("Enter the point for y1 :");
-	 double y1 = sc.nextDouble();
-	 System.out.println("Enter the point for x2 :");
-	 double x2 = sc.nextDouble();
-	 System.out.println("Enter the point for y2 :");
-	 double y2 = sc.nextDouble();
-	 
-	 result1=obj.Calculate_length(x1,y1,x2,y2);
+	 result1 = obj.input();
 	 System.out.println("calculated length for 1st Line :" + result1);
 	 //line 2
 	 System.out.println("Line 2 points");
-	 System.out.println("Enter the point for x1 :");
-	 double a1 = sc.nextDouble();
-	 System.out.println("Enter the point for y1 :");
-	 double b1 = sc.nextDouble();
-	 System.out.println("Enter the point for x2 :");
-	 double a2 = sc.nextDouble();
-	 System.out.println("Enter the point for y2 :");
-	 double b2 = sc.nextDouble();
-	 
-	 result2 = obj.Calculate_length(a1,b1,a2,b2);
+        result2 = obj1.input();
 	 System.out.println("calculated length for 2nd Line :" + result2);
 	 
 	 if(result1.equals(result2)) {
@@ -59,6 +54,7 @@ public class CartesianSystem {
 		
 		System.out.println("Line 1 is greater than line 2");   // 1 for greater
 	  }
+	
 	
 	 
 	}
