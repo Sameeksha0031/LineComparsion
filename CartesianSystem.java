41cd33e7614fe8a488115e5aaac1ac2ae1cade5f
Akash <akhvnd@gmail.com>
2022-07-17 22:27:02 +0530

    modified:using the Cartesian system

--- CartesianSystem.java
+++ CartesianSystem.java
@@ -2,14 +2,29 @@ package com.lineSystem.java;
 
 import java.util.Scanner;
 
-//import java.lang.Math.*;
 public class CartesianSystem {
 
-    //int x1,x2,y1,y2;	
-
+   
 	public static void main(String[] args) {
 		
 	 System.out.println("Welcome to Line Comparison Computation ");
-	}  
-
-}
+	 Scanner sc = new Scanner(System.in);
+	 System.out.println("Enter the point for x1 :");
+	 double x1 = sc.nextDouble();
+	 System.out.println("Enter the point for y1 :");
+	 double y1 = sc.nextDouble();
+	 System.out.println("Enter the point for x2 :");
+	 double x2 = sc.nextDouble();
+	 System.out.println("Enter the point for y2 :");
+	 double y2 = sc.nextDouble();
+	 CartesianSystem obj = new CartesianSystem();
+	 obj.Calculate_length(x1,y1,x2,y2);
+	}
+	
+	 public double Calculate_length(double x1,double y1,double x2, double y2){
+		 double len;
+		 len=Math.sqrt((Math.pow(x2-x1,2)) + Math.pow(y2-y1,2));
+		 System.out.println("calculated length:" + len);
+	 return len;
+	 }
+}  
